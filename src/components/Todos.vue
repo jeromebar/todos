5702da7a21c2514b846a1b0ce44f944f1fe882b6
jbar <jerome.bar@prestataires.boulanger.com>
2018-11-23 10:50:16 +0100

    version initiale du projet

---
+++ src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="todoapp">
+    <header class="header">
+      <h1>Todos</h1>
+      <input type="text" class="new-todo" placeholder="Ajouter une tâche">
+    </header>
+    <div class="main">
+      <ul class="todo-list">
+        <li></li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style src="./todos.css"></style>
